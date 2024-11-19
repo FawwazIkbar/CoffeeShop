2b056fc4bcf9fcc24657148ff06c1a9809765711
FawwazIkbar <fawwazcanaiago@gmail.com>
2024-11-19 13:00:44 +0700

    menambahkan fitur memesan via wa

--- js/app.js
+++ js/app.js
@@ -64,6 +64,51 @@ document.addEventListener("alpine:init", () => {
   });
 });
 
+// form validasi
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+/* kirim data ketika cheouut button di klik */
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form); //ambil form data dari form
+  const data = new URLSearchParams(formData); //ambil data dari URL
+  const objData = Object.fromEntries(data); //ubah data ke objek
+  const message = formatMessage(objData);
+  // masukkan nomor hp owner
+  window.open(`http://wa.me/6282173398603?text=` + encodeURIComponent(message));
+});
+
+// format wa
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama  : ${obj.name}
+  Email : ${obj.email}
+  Phone : ${obj.phone}
+
+  Data Pesanan : 
+  ${JSON.parse(obj.items).map((item) => `${item.name} (${item.totalBarang} X ${rupiah(item.totalHarga)})`)}
+
+  TOTAL Harga : ${rupiah(obj.totalHarga)}
+  TERIMAKASIH:)
+  `;
+};
+
 // konversi ke rupiah
 
 const rupiah = (number) => {
